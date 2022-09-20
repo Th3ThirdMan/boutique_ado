e160d2e6ac601c65c123d46fa9b3c77bee5c3e39
Th3ThirdMan <davidgkennedymail@gmail.com>
2022-09-20 14:15:19 +0000

    Complete homepage header and CSS

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'home'
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -147,6 +147,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
